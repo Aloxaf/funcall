4f3a214c173fe8993f2d3e166e24749a66c10689
Aloxaf <aloxafx@gmail.com>
2019-05-08 12:35:52 +0800

    32位 cdecl / stdcall

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,47 @@
 
 #![feature(asm)]
 
+use std::any::{Any, TypeId};
 use std::ffi::OsStr;
 use std::mem;
 
-use num_traits::Num;
-use std::any::{Any, TypeId};
+/// 可以被当成参数传递的类型
+/// 包含裸指针和 primitive 类型
+pub trait Arg {}
+
+impl<T> Arg for *const T {}
+
+impl<T> Arg for *mut T {}
+
+macro_rules! impl_arg {
+    ($($ty:ty), *) => {
+        $(impl Arg for $ty {})*
+    };
+}
+
+impl_arg!(i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize, f32, f64);
 
 type Result<T> = std::io::Result<T>;
 
-/// cdecl 调用约定, C 函数默认使用的调用约定
+/// # Example
 ///
-/// TODO: 示例代码
+/// ```
+/// use apicall::Func;
 ///
-/// # 传参方式
-///
-/// - 32位: 参数从右至左依次压栈, 不足 32 位的补齐 32 位, 超过 32 位的由高到低依次压栈
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+/// let mut func = Func::new("/usr/lib32/libc.so.6", b"printf\0").unwrap();
+/// func.push(b"%d".as_ptr());
+/// func.push(2233);
+/// unsafe {
+///     func.cdecl();
+/// }
+/// ```
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct Func {
     func: *const fn(),
     args: Vec<usize>,
+    ret_low: usize,
+    ret_high: usize,
+    ret_float: f64,
 }
 
 impl Func {
@@ -33,12 +55,16 @@ impl Func {
             Ok(Self {
                 func: *func.into_raw() as *const fn(),
                 args: Vec::new(),
+                ret_low: 0,
+                ret_high: 0,
+                ret_float: 0.0,
             })
         }
     }
 
-    /// 压入基本数字类型参数
-    pub fn push<T: Num + Any>(&mut self, arg: T) {
+    // TODO: 限制参数类型: primitive 类型或 裸指针
+    /// 压入参数
+    pub fn push<T: Arg + Any>(&mut self, arg: T) {
         // 浮点数理应使用浮点专用的指令传参, 然而咱是手动压栈的, 压栈的时候已经丢失了类型信息, 只能用 push
         // 所以需要手动转成适合压栈的格式, 对于 f64 来说, 规则和 u64 一样, 但是 f32 需要先转成 f64 再压栈(即对齐
         if arg.type_id() == TypeId::of::<f32>() {
@@ -62,25 +88,20 @@ impl Func {
             // 当参数大小大于机器字长, 如 32 位下的 f64 时
             // 分割为一个 &[usize] 再压入
             let len = mem::size_of::<T>() / mem::size_of::<usize>();
-            let slice = unsafe {
-                std::slice::from_raw_parts(&arg as *const _ as *const usize, len)
-            };
+            let slice =
+                unsafe { std::slice::from_raw_parts(&arg as *const _ as *const usize, len) };
             self.args.extend_from_slice(slice);
         }
     }
 
-    /// 压入指针类型的参数
-    pub fn push_ref<T>(&mut self, arg: &T) {
-        self.args.push(unsafe { mem::transmute_copy::<T, usize>(&arg) });
-    }
-
-    // TODO: 返回值
+    /// 以 cdecl 调用约定调用函数
+    /// 即 C 语言默认使用的调用约定
     #[cfg(target_arch = "x86")]
-    pub unsafe fn cdecl_call(&self) {
+    pub unsafe fn cdecl(&mut self) {
         let mut low: usize;
         let mut high: usize;
-        let mut double: f64 = 0.0; // TODO:
-        // 参数从右往左入栈, 因此先取得最右边的地址
+        let mut double: f64; // TODO:
+                             // 参数从右往左入栈, 因此先取得最右边的地址
         let end_of_args = self.args.as_ptr().offset(self.args.len() as isize - 1);
         asm!(r#"
             mov edi, ebx  // 备份参数个数
@@ -97,30 +118,90 @@ impl Func {
             shl edi, 2    // 参数个数x4, 得到恢复堆栈指针所需的大小
             add esp, edi  // 恢复堆栈指针
             "#
-            : "={eax}"(low) "={edx}"(high) //"=f"(double)
+            : "={eax}"(low) "={edx}"(high) "={st}"(double) // https://github.com/rust-lang/rust/issues/20213
             : "{eax}"(end_of_args) "{ebx}"(self.args.len()) "{ecx}"(self.func)
             : "eax" "ebx" "ecx" "edx"
             : "intel");
-        println!("{} {} {}", low, high, double);
+        self.ret_low = low;
+        self.ret_high = high;
+        self.ret_float = double;
+    }
+
+    /// 以 stdcall 调用约定调用函数
+    /// 即 WINAPI 使用的调用约定
+    #[cfg(target_arch = "x86")]
+    pub unsafe fn stdcall(&mut self) {
+        let mut low: usize;
+        let mut high: usize;
+        let mut double: f64; // TODO:
+        // 参数从右往左入栈, 因此先取得最右边的地址
+        let end_of_args = self.args.as_ptr().offset(self.args.len() as isize - 1);
+        asm!(r#"
+            mov edi, ebx  // 备份参数个数
+
+            dec ebx       // 将 ecx 个参数依次压栈
+            LOOP:
+            push dword ptr [eax]
+            sub eax, 4
+            dec ebx
+            jns LOOP
+
+            call ecx  // 调用函数
+            "#
+            : "={eax}"(low) "={edx}"(high) "={st}"(double) // https://github.com/rust-lang/rust/issues/20213
+            : "{eax}"(end_of_args) "{ebx}"(self.args.len()) "{ecx}"(self.func)
+            : "eax" "ebx" "ecx" "edx"
+            : "intel");
+        self.ret_low = low;
+        self.ret_high = high;
+        self.ret_float = double;
+    }
+
+    pub fn ret_f64(&self) -> f64 {
+        self.ret_float
+    }
+
+    pub fn ret_usize(&self) -> usize {
+        self.ret_low
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ffi::CStr;
 
     #[test]
     #[cfg(target_arch = "x86")]
     fn cdecl() {
-        let mut func = Func::new("/usr/lib32/libc.so.6", b"printf\0").unwrap();
-        func.push_ref(&b"%d %lld %f %f\0");
+        let mut buf = vec![0i8; 100];
+        let mut func = Func::new("/usr/lib32/libc.so.6", b"sprintf\0").unwrap();
+        func.push(buf.as_mut_ptr());
+        func.push(b"%d %lld %.3f %.3f\0".as_ptr());
         func.push(2233i32);
         func.push(2147483648i64);
         func.push(2233.0f32);
         func.push(123.456f64);
-        println!("{:?}", func);
         unsafe {
-            func.cdecl_call();
+            func.cdecl();
+            assert_eq!(
+                CStr::from_ptr(buf.as_ptr()).to_str().unwrap(),
+                "2233 2147483648 2233.000 123.456"
+            );
         }
+
+        let mut func = Func::new("/usr/lib32/libc.so.6", b"atoi\0").unwrap();
+        func.push(b"2233\0".as_ptr());
+        unsafe {
+            func.cdecl();
+        }
+        assert_eq!(func.ret_usize(), 2233);
+
+        let mut func = Func::new("/usr/lib32/libc.so.6", b"atof\0").unwrap();
+        func.push(b"123.456\0".as_ptr());
+        unsafe {
+            func.cdecl();
+        }
+        assert!(func.ret_f64() - 123.456 <= std::f64::EPSILON);
     }
 }
